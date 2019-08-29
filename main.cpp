db1d663e97a8a89ad2eeac3ef4189ad866fedd2f
Hosseinali Salemi <41801628+halisalemi@users.noreply.github.com>
2019-08-29 10:10:21 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 
 
 	/*Preprocessing procedure to find the non-critical nodes
-	/*It works for arbitrary k and b and also for edge-weighted graphs*/
+	/*It works for arbitrary k and b*/
 	else if (strcmp(argv[1], "FindNonCriticalNodes") == 0)
 	{
 		KGraph g(argv[3], argv[3], argv[2]);
@@ -77,145 +77,183 @@ int main(int argc, char *argv[])
 	}
 
 
-	/*To solve DCNP when distances are measured in terms of hops
+	/*To solve DCNP for graphs with hop-based distances.
 	* Thin formulation with integer separation is used. */
-	else if (strcmp(argv[1], "Thin") == 0)
+	else if (strcmp(argv[1], "Thin_I") == 0)
 	{
 		KGraph g(argv[3], argv[3], argv[2]);
-
 		long k = atol(argv[4]); //short distance
 		long b = atol(argv[5]); //budget 
+		cout << g.name << " Thin_I " << k << " " << b << " ";
 		time_t start = clock();
 
+		cerr << "Finding heuristic solution" << endl;
 		time_t Heuristic_start = clock();
 		vector<long> Heuristic_sol = DCNP_Heuristic(g, k, b);
 		double Heuristic_time = (double)(clock() - Heuristic_start) / CLOCKS_PER_SEC;
 		cerr << "Heuristic time = " << Heuristic_time << endl;
-		
-		vector<long> criticalNodes = solveDCNP_thin_formulation(g, k, b, Heuristic_sol);
 
-		cout << "Total time = " << (double)(clock() - start) / CLOCKS_PER_SEC << endl;
-		cerr << "# critical nodes = " << criticalNodes.size() << endl;
-		cerr << "** Label of critical node(s) = ";
-		PrintVectorLong(criticalNodes);
-		cerr << "# close vertex pairs in G-D = " << obj(g, criticalNodes, k) << endl;
+		bool subopt;
+		vector<long> criticalNodes = Thin_I(g, k, b, Heuristic_sol, subopt);
+		cout << "Total time = " << (double)(clock() - start) / CLOCKS_PER_SEC << " ";
+
+		if (subopt) cout << "Model is infeasible or other problem." << endl;
+		else
+		{
+			cerr << "# critical nodes = " << criticalNodes.size() << endl;
+			cerr << "** Label of critical node(s) = ";
+			PrintVectorLong(criticalNodes);
+			cout << "# close vertex pairs in G-D = " << obj(g, criticalNodes, k) << endl;
+		}
 	}
 
-
-	/*To solve DCNP in edge-weighted graphs.
-	* Thin formulation with integer separation is used.*/
-	else if (strcmp(argv[1], "Thin_weighted") == 0)
+	/*To solve DCNP for graphs with hop-based distances.
+	* Thin formulation with fractional separation is used. */
+	else if (strcmp(argv[1], "Thin_F") == 0)
 	{
 		KGraph g(argv[3], argv[3], argv[2]);
-
 		long k = atol(argv[4]); //short distance
 		long b = atol(argv[5]); //budget 
+		cout << g.name << " Thin_F " << k << " " << b << " ";
 		time_t start = clock();
+
 		cerr << "Finding heuristic solution" << endl;
 		time_t Heuristic_start = clock();
-		vector<long> Heuristic_sol = DCNP_Heuristic_Weighted(g, k, b);
-		double Heuristic_time = (double)(clock() - Heuristic_start) / CLOCKS_PER_SEC;
-		cout << "Heuristic time = " << Heuristic_time << endl;
-		
-		vector<long> criticalNodes = solveDCNP_thin_formulation_weighted(g, k, b, Heuristic_sol);
-
-		cout << "Total time = " << (double)(clock() - start) / CLOCKS_PER_SEC << endl;
-		cerr << "# critical nodes = " << criticalNodes.size() << endl;
-		cerr << "** Label of critical node(s) = ";
-		PrintVectorLong(criticalNodes);
-		cerr << "# close vertex pairs in G-D = " << obj_weighted(g, criticalNodes, k) << endl;
-	}
-
-
-	/*To solve DCNP when distances are measured in terms of hops
-	* Thin formulation with fractional separation is used. */
-	else if (strcmp(argv[1], "Thin_fractional") == 0)
-	{
-		KGraph g(argv[3], argv[3], argv[2]);
-
-		long k = atol(argv[4]); //short distance
-		long b = atol(argv[5]); //budget 
-		time_t start = clock();
-
-		time_t Heuristic_start = clock();
 		vector<long> Heuristic_sol = DCNP_Heuristic(g, k, b);
-		//vector<long> Heuristic_sol;
 		double Heuristic_time = (double)(clock() - Heuristic_start) / CLOCKS_PER_SEC;
-		cout << "Heuristic time = " << Heuristic_time << endl;
-		
+		cerr << "Heuristic time = " << Heuristic_time << endl;
 
-		vector<long> criticalNodes = solveDCNP_thin_formulation_fractional(g, k, b, Heuristic_sol);
+		bool subopt;
+		vector<long> criticalNodes = Thin_F(g, k, b, Heuristic_sol, subopt);
+		cout << "Total time = " << (double)(clock() - start) / CLOCKS_PER_SEC << " ";
 
-		cout << "Total time = " << (double)(clock() - start) / CLOCKS_PER_SEC << endl;
-		cerr << "# critical nodes = " << criticalNodes.size() << endl;
-		cerr << "** Label of critical node(s) =  ";
-		PrintVectorLong(criticalNodes);
-		cerr << "# close vertex pairs in G-D = " << obj(g, criticalNodes, k) << endl;
+		if(subopt) cout << "Model is infeasible or other problem." << endl;
+		else
+		{
+			cerr << "# critical nodes = " << criticalNodes.size() << endl;
+			cerr << "** Label of critical node(s) =  ";
+			PrintVectorLong(criticalNodes);
+			cout << "# close vertex pairs in G-D = " << obj(g, criticalNodes, k) << endl;
+		}
 	}
 
 
-	/*To solve DCNP when distances are measured in terms of hops and k=3 
+	/*To solve DCNP when distances are measured in terms of hops and k=3. 
 	 * Path-like formulation is used */
 	else if (strcmp(argv[1], "Path_like_k3") == 0)
 	{
 		KGraph g(argv[3], argv[3], argv[2]);
-
+		long k = 3;
 		long b = atol(argv[4]); //budget 
+		cout << g.name << " Path_like_k3 3 " << b << " ";
 		time_t start = clock();
 
-		time_t Heuristic_start = clock();
-		vector<long> Heuristic_sol = DCNP_Heuristic(g, 3, b);
-		double Heuristic_time = (double)(clock() - Heuristic_start) / CLOCKS_PER_SEC;
-		cout << "Heuristic time = " << Heuristic_time << endl;
-		
-
-		vector<long> criticalNodes = solveDCNP_path_like_k3(g, b, Heuristic_sol);
-
-		cout << "Total time = " << (double)(clock() - start) / CLOCKS_PER_SEC << endl;
-		cerr << "# critical nodes = " << criticalNodes.size() << endl;
-		cerr << "** Label of critical node(s) =  ";
-		PrintVectorLong(criticalNodes);
-		cerr << "# close vertex pairs in G-D = " << obj(g, criticalNodes, 3) << endl;
-	}
-
-
-	/*To solve DCNP when distances are measured in terms of hops
-	* Recursive formulation is used. */
-	else if (strcmp(argv[1], "DCNP_Veremyev") == 0)
-	{
-		KGraph g(argv[3], argv[3], argv[2]);
-
-		long k = atol(argv[4]); //short distance
-		long b = atol(argv[5]); //budget 
-		time_t start = clock();
-
+		cerr << "Finding heuristic solution" << endl;
 		time_t Heuristic_start = clock();
 		vector<long> Heuristic_sol = DCNP_Heuristic(g, k, b);
 		double Heuristic_time = (double)(clock() - Heuristic_start) / CLOCKS_PER_SEC;
 		cerr << "Heuristic time = " << Heuristic_time << endl;
 		
-		vector<long> criticalNodes = solveDCNP_Veremyev(g, k, b, Heuristic_sol);
+		bool subopt;
+		vector<long> criticalNodes = Path_like_k3(g, b, Heuristic_sol, subopt);
+		cout << "Total time = " << (double)(clock() - start) / CLOCKS_PER_SEC << " ";
 
-		cout << "Total time = " << (double)(clock() - start) / CLOCKS_PER_SEC << endl;
-		cerr << "# critical nodes = " << criticalNodes.size() << endl;
-		cerr << "** Label of critical node(s) =  ";
-		PrintVectorLong(criticalNodes);
-		cerr << "# close vertex pairs in G-D = " << obj(g, criticalNodes, k) << endl;
+		if (subopt) cout << "Model is infeasible or other problem." << endl;
+		else
+		{
+			cerr << "# critical nodes = " << criticalNodes.size() << endl;
+			cerr << "** Label of critical node(s) =  ";
+			PrintVectorLong(criticalNodes);
+			cout << "# close vertex pairs in G-D = " << obj(g, criticalNodes, k) << endl;
+		}
 	}
 
-	else if (strcmp(argv[1], "Diameter") == 0)
+
+	/*To solve DCNP when distances are measured in terms of hops.
+	* Recursive formulation is used. */
+	else if (strcmp(argv[1], "Recursive") == 0)
 	{
 		KGraph g(argv[3], argv[3], argv[2]);
-		long diameter = g.DiameterWeighted();
-		cerr << "diameter = " << diameter << endl;
-		vector< vector< long> > clusters;
-		vector<long> degreeZero;
-		g.FindConnectedComponents(clusters, degreeZero);
-		cerr << "# isolated vertices " << degreeZero.size() << endl;
-		for (long i = 0; i < degreeZero.size(); i++)
-			cerr << degreeZero[i] << endl;
-		cerr << "# components " << clusters.size() << endl;
+		long k = atol(argv[4]); //short distance
+		long b = atol(argv[5]); //budget
+		cout << g.name << " Recursive " << k << " " << b << " ";
+		time_t start = clock();
+
+		cerr << "Finding heuristic solution" << endl;
+		time_t Heuristic_start = clock();
+		vector<long> Heuristic_sol = DCNP_Heuristic(g, k, b);
+		double Heuristic_time = (double)(clock() - Heuristic_start) / CLOCKS_PER_SEC;
+		cerr << "Heuristic time = " << Heuristic_time << endl;
+		
+		bool subopt;
+		vector<long> criticalNodes = Recursive(g, k, b, Heuristic_sol, subopt);
+		cout << "Total time = " << (double)(clock() - start) / CLOCKS_PER_SEC << " ";
+
+		if (subopt) cout << "Model is infeasible or other problem." << endl;
+		else
+		{
+			cerr << "# critical nodes = " << criticalNodes.size() << endl;
+			cerr << "** Label of critical node(s) =  ";
+			PrintVectorLong(criticalNodes);
+			cout << "# close vertex pairs in G-D = " << obj(g, criticalNodes, k) << endl;
+		}
 	}
+
+
+	/*To solve DCNP for graphs with edge-weighted distances.
+	* Thin formulation with integer separation is used.*/
+	else if (strcmp(argv[1], "Thin_weighted") == 0)
+	{
+		KGraph g(argv[3], argv[3], argv[2]);
+		long k = atol(argv[4]); //short distance
+		long b = atol(argv[5]); //budget 
+		cout << g.name << " " << k << " " << b << " ";
+		time_t start = clock();
+
+		cerr << "Finding heuristic solution" << endl;
+		time_t Heuristic_start = clock();
+		vector<long> Heuristic_sol = DCNP_Heuristic_Weighted(g, k, b);
+		double Heuristic_time = (double)(clock() - Heuristic_start) / CLOCKS_PER_SEC;
+		cout << "Heuristic time = " << Heuristic_time << " ";
+
+		bool subopt;
+		vector<long> criticalNodes = Thin_Weighted(g, k, b, Heuristic_sol, subopt);
+		cout << "Total time = " << (double)(clock() - start) / CLOCKS_PER_SEC << " ";
+
+		if (subopt) cout << "Model is infeasible or other problem." << endl;
+		else
+		{
+			cerr << "# critical nodes = " << criticalNodes.size() << endl;
+			cerr << "** Label of critical node(s) = ";
+			PrintVectorLong(criticalNodes);
+			cout << "# close vertex pairs in G-D = " << obj_weighted(g, criticalNodes, k) << endl;
+		}
+	}
+
+
+	else if (strcmp(argv[1], "FindPercentiles") == 0)
+	{
+		KGraph g(argv[3], argv[3], argv[2]);
+		double alpha = atol(argv[4]); //We'll consider alpha^th percentile
+		vector<vector<long>> DistanceMatrix;
+		for (long i = 0; i < g.n; i++)
+		{
+			vector<long> dist_from_i = g.ShortestPathsWeighted(i);
+			DistanceMatrix.push_back(dist_from_i);
+		}
+		vector<long> AllDist;
+		for (long i = 0; i < g.n; i++)
+		{
+			for (long j = i + 1; j < g.n; j++)
+			{
+				AllDist.push_back(DistanceMatrix[i][j]);
+			}
+		}
+		sort(AllDist.begin(), AllDist.end());
+		int index = int(double(alpha / 100)*AllDist.size());
+		cerr << "k = " << AllDist[index];
+	}
+
 
 	else
 	{
